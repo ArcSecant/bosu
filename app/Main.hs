fcf8e694e3c0fa1d7208bdade849a6026ae10780
ArcSecant <lightlance7@gmail.com>
2021-01-08 03:06:44 -0800

    sliders

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 
 module Main where
 
+import Data.List
 import qualified Data.Vector as V
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -19,38 +20,113 @@ import Text.Megaparsec
 import Debug.Trace
 import Graphics.Gloss
 
+import Lib
+
 -- type FileResults = Maybe (Either (FormatVersion, ParseErrorBundle T.Text (ErrorItem T.Text)) G.Beatmap)
 data VHO = Vector HitObject
+type BInfo = (Float, [TimingPoint], [HitObject])
 
-hCircle :: (Int, Int) -> Float -> Picture
-hCircle (x, y) r = Translate (fromIntegral x) (fromIntegral y)  (Color cyan $ circleSolid r)
+audioPath = "C:\\Program Files (x86)\\osu!\\Songs\\1199834 Vickeblanka - Black Rover (TV Size)\\audio.mp3"
+mapPath = "C:\\Program Files (x86)\\osu!\\Songs\\1199834 Vickeblanka - Black Rover (TV Size)\\Vickeblanka - Black Rover (TV Size) (IOException) [Special].osu"
 
-aCircle :: (Int, Int) -> Float -> Picture
-aCircle (x, y) r = Translate (fromIntegral x) (fromIntegral y)  (Color white $ Circle r)
+sliderRate :: Float -> Float -> [TimingPoint] -> Float
+sliderRate curTime sm tps' = let tps = reverse $ filter (\x -> beatLength x <= 0) tps' in
+    sliderRate' tps
+    where sliderRate' ts = if null ts then 340*sm else if curTime*1000 >= (realToFrac $ offset (head ts)) then 340*(-100)*sm/(realToFrac $ beatLength (head ts)) else sliderRate' (tail ts)
+    -- where sliderRate' tpoints = let t:ts = tpoints in if null tpoints then 350*sm else case null ts of
+    --         True -> 350*(-100)*sm/(realToFrac $ beatLength t)
+    --         _ -> if curTime*1000 >= (realToFrac $ offset t) && curTime*1000 < (realToFrac $ offset (head ts)) then 350*(-100)*sm/(realToFrac $ beatLength t) else sliderRate' (tail ts)
+    
+getSec :: HitObject -> Float
+getSec x = (fromIntegral $ time x) / 1000
 
-audioPath = "C:\\Program Files (x86)\\osu!\\Songs\\1202012 Magus Two - BEAUTIFUL SENTENCE (TV Size)\\audio.mp3"
-mapPath = "C:\\Program Files (x86)\\osu!\\Songs\\1202012 Magus Two - BEAUTIFUL SENTENCE (TV Size)\\Magus Two - BEAUTIFUL SENTENCE (TV Size) (Sephira) [Luxuria].osu"
-
-parseFile :: FilePath -> T.Text -> [HitObject]
+parseFile :: FilePath -> T.Text -> BInfo
 parseFile path content = case parse beatmap path content of
-    Left  e -> []
-    Right b -> hitObjects b
+    Left  e -> (0, [], [])
+    Right b -> let sm = sliderMultiplier b in case sm of
+        Nothing -> (1.4, timingPoints b, hitObjects b)
+        Just a -> (realToFrac a, timingPoints b, hitObjects b)
 
 windowDisplay :: Display
-windowDisplay = InWindow "BOsu" (1080, 720) (10, 10)
+windowDisplay = InWindow "BOsu" (1080, 720) (0, 0)
 
-animationFunc :: Float -> [HitObject] -> Float -> Picture
-animationFunc ar hitobjs t = Pictures ((map (\ho -> hCircle (position ho) 40) visibleObjs) ++ approachCircs)
-    where
-        visibleObjs = filter (\x -> (fromIntegral $ time x)/1000 <= t + ar && (fromIntegral $ time x)/1000 >= t) hitobjs 
-        approachCircs = map (\x -> aCircle (position x) $ 40.0 * (1.0 + 2.0 * ((fromIntegral $ time x)/1000 - t) / ar)) visibleObjs
+toFloatPair :: (Int, Int) -> (Float, Float)
+toFloatPair (a, b) = (fromIntegral a, fromIntegral b)
+
+toVec :: [Obmapp.Beatmap.Point] -> Bezier
+toVec xs = map toFloatPair xs
+
+getHitCircles :: Float -> BInfo -> [Picture]
+getHitCircles t (sm, tps, hitObjs) = map renderHCircle hitObjs where
+    renderHCircle ho = case details ho of
+        HitCircle -> hCircle (position ho) 40
+        Spinner _ -> hCircle (256, 192) 300
+        Slider shape _ _ -> case shape of
+            Linear points -> drawBezier $ map reCenter $ (toFloatPair $ position ho):(toVec points)
+            Bezier points -> Pictures $ map (drawBezier . map reCenter) $ [newHead] ++ (map toVec $ tail points)
+                where newHead = (toFloatPair $ position ho):(toVec $ head points)
+            Perfect a b -> let ((x, y), r) = circumcircle (toFloatPair $ position ho) (toFloatPair a) (toFloatPair b) in
+                Translate (x - 256) (y - 192) (Color cyan $ ThickCircle r 40)
+            _ -> hCircle (position ho) 40
+        where (a, b) = toFloatPair $ position ho
+
+getVisibleObjs :: Float -> BInfo -> [HitObject]
+getVisibleObjs curTime (sm, tps, hitObjs) = filter isVisible hitObjs where
+    isVisible ho = case details ho of
+        Spinner endtime -> getSec ho - 0.5 <= curTime + 0.5 && fromIntegral endtime / 1000 >= curTime
+        HitCircle -> getSec ho - 0.5 <= curTime && getSec ho >= curTime
+        Slider _ info pLength -> getSec ho - 0.5 <= curTime && getSec ho + (fromIntegral $ repeats info)*(realToFrac pLength)/(sliderRate (getSec ho) sm tps) >= curTime
+
+getSliderball :: Float -> BInfo -> [Picture]
+getSliderball curTime (sm, tps, hitObjs) = map renderSlider hitObjs where
+    renderSlider ho = case details ho of
+        Slider shape info pLength -> case shape of
+            Linear points -> makeSliderball ho $ (toFloatPair $ position ho):(toVec points)
+            Bezier points -> makeSliderball ho $ concat $ map getBezierPath $ [newHead] ++ (map toVec $ tail points)
+                where newHead = (toFloatPair $ position ho):(toVec $ head points)
+            _ -> Blank
+            where
+                makeSliderball ho ps = let
+                    path' = getBezierPath ps
+                    path'' n =
+                        if even n then concat $ take (n `div` 2) $ repeat $ path' ++ (reverse path')
+                        else (path'' (n-1)) ++ path'
+                    path = V.fromList $ map head $ group $ path'' $ repeats info
+                    lPath = V.length path
+                    lastsFor = (fromIntegral $ repeats info)*(realToFrac pLength :: Float) / (sliderRate curTime sm tps)
+                    deltaT = lastsFor / (fromIntegral lPath)
+                    curIdx = let t = curTime - getSec ho in if t >= 0 then min (floor $ t/deltaT) (lPath - 1) else 0
+                    (curX, curY) = path V.! curIdx
+                    in Translate (curX-256) (curY-192) $ Color white $ ThickCircle 35 7.5
+        _ -> Blank
     
+-- reCenter :: HitObject -> HitObject
+-- reCenter p@(HitObject {position = (x, y)}) = p {position = (x - 256, y - 192)}
+
+animationFunc :: BInfo -> Float -> Picture
+animationFunc binfo t = Pictures (hitCircles ++ approachCircs ++ sliderBall)
+    where
+        (sm, tps, hitObjs) = binfo 
+        visibleObjs = getVisibleObjs t binfo
+        sliderBall = getSliderball t (sm, tps, visibleObjs)
+        hitCircles = getHitCircles t (sm, tps, visibleObjs)
+        approachCircs = map (getApprachCirc) visibleObjs
+        getApprachCirc ho = let r = (max 1 (1.0 + 2.0 * (getSec ho - t) / 0.5)) in
+            case details ho of
+                Spinner _ -> Blank
+                _ -> if r == 1 then Blank else aCircle (position ho) (40.0 * r)
+
+mainTest t = do
+    file <- T.readFile mapPath
+    let (a, b, c) = parseFile mapPath file in
+        putStrLn $ show $ sliderRate t 1 b
+
 main :: IO ()
 main = do
     SDL.initialize [SDL.InitAudio]
     res <- Mixer.openAudio Mixer.defaultAudio 4096
-    play <- Mixer.load audioPath
-    p <- Mixer.playForever play
+    audio <- Mixer.load audioPath
+    p <- Mixer.play audio
     file <- T.readFile mapPath
     let curBMap = parseFile mapPath file in
-        animate windowDisplay black $ animationFunc 0.5 curBMap
+        animate windowDisplay black $ animationFunc curBMap
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,69 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Graphics.Gloss
+import Graphics.Gloss.Data.Vector
+
+type Bezier = [(Float, Float)]
+
+circumcircle :: Vector -> Vector -> Vector -> (Vector, Float)
+circumcircle (x1, y1) (x2, y2) (x3, y3) = let
+    d = 2 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+    ux = ((x1 * x1 + y1 * y1) * (y2 - y3) + (x2 * x2 + y2 * y2) * (y3 - y1) + (x3 * x3 + y3 * y3) * (y1 - y2)) / d
+    uy = ((x1 * x1 + y1 * y1) * (x3 - x2) + (x2 * x2 + y2 * y2) * (x1 - x3) + (x3 * x3 + y3 * y3) * (x2 - x1)) / d
+    center = (ux, uy)
+    in (center, dist center (x1, y1))
+    where
+        dist (a1, b1) (a2, b2) = sqrt $ (a2 - a1) ** 2 + (b2 - b1) ** 2
+
+reCenter :: (Float, Float) -> (Float, Float)
+reCenter (a, b) = (a - 256, b - 192)
+
+addV :: Vector -> Vector -> Vector
+addV (a, b) (c, d) = (a + c, b + d)
+
+subV :: Vector -> Vector -> Vector
+subV (a,b) (c,d) = (a - c,b - d)
+
+makePairs :: [a] -> [(a, a)]
+makePairs ps = let
+    ps' = take (length ps - 1) ps
+    ps'' = drop 1 ps
+    in zip ps' ps''
+
+listify :: (a, a) -> [a]
+listify (a, b) = [a, b]
+
+bezier :: Float -> Bezier -> Vector
+bezier u (p1:p2:[]) = addV p1 $ mulSV u (subV p2 p1)
+bezier u ps         = let pairs = map listify $ makePairs ps in bezier u $ (map (bezier u)) pairs
+
+hCircle :: (Int, Int) -> Float -> Picture
+hCircle (x, y) r = Translate (fromIntegral x - 256) (fromIntegral y - 192) (Color cyan $ ThickCircle (r - 5) 7.5)
+
+aCircle :: (Int, Int) -> Float -> Picture
+aCircle (x, y) r = Translate (fromIntegral x - 256) (fromIntegral y - 192) $ Color white $ ThickCircle r 2.5
+
+lineToCirc :: Vector -> Picture
+lineToCirc (a, b) = Translate a b (Color cyan $ circleSolid 40)
+
+getBezierPath :: Bezier -> [Vector]
+getBezierPath ps = let
+    p1 = head ps
+    pn = last ps
+    nsteps = (magV $ subV pn p1) / 10
+    in map (\u -> bezier u ps) [0, (min 0.1 (1/nsteps)) .. 1]
+
+drawBezier :: Bezier -> Picture
+drawBezier ps = Color cyan $ Pictures $ map lineToCirc $ getBezierPath ps
+-- drawBezier :: Bezier -> Picture
+-- drawBezier ps = Pictures [Color cyan $ Line $ map f $ getBezierPath ps, Color cyan $ Line $ map g $ getBezierPath ps]
+--     where
+--         f (a, b) = (a - 40, b - 40)
+--         g (a, b) = (a + 40, b + 40)
+
+
+-- test = do
+--     display
+--         (InWindow "Bézier" (600,600) (100,100))
+--         white
+--         (drawBezier [(0,0),(200,0)])
